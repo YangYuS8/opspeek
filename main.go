440c0392542bdb834af1d723ee6df9efc56b2c5d
YangYuS8 <YangYuS8@163.com>
2026-04-15 21:29:08 +0800

    添加 Docker 状态检查功能

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
+
+func checkDocker() string {
+	cmd := exec.Command("docker", "info")
+	err := cmd.Run()
+	if err != nil {
+		return "NOT AVAILABLE"
+	}
+	return "RUNNING"
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("=== opspeek ===")
+	fmt.Println("Docker:", checkDocker())
 }
